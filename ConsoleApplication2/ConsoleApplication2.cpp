51c1eaa47b2b91892dd3339637a3158fd50538fc
nikit <nikit@DESKTOP-5363RTP>
2023-05-02 17:33:42 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+class Pen {
+private:
+	string Color;
+public:
+	string getColor();
+	void setColor(string newColor)
+	{
+		if (newColor.length() != 6)
+			Color = "000000";	
+		else Color = newColor;
+	}
+
+};
+string Pen::getColor() { return Color; }
+
+
+int main() {
+	Pen pen;
+	pen.setColor("FFFFFF");
+	cout << pen.getColor() << endl;
+
+	return 0;
+}
